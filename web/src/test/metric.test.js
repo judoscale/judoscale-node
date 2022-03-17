4f99be853cdf0c31258347f495e8715150ba7400
Carlos Marques <carlos.mattos@fundingcircle.com>
2022-03-17 13:08:48 -0300

    Test coverage for metric.js

---
+++ web/src/test/metric.test.js
@@ -0,0 +1,14 @@
+/* global test, expect */
+
+import Metric from '../lib/metric'
+
+test('Metric class', () => {
+  const identifier = 'some-identifier'
+  const time = new Date()
+  const value = '1234'
+  const metric = new Metric(identifier, time, value)
+
+  expect(metric.identifier).toBe(identifier)
+  expect(metric.time).toEqual(new Date(time.toUTCString()))
+  expect(metric.value).toBe(1234)
+})
