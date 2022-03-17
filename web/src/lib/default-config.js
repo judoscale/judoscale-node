7947a1cd67a891b722a2e61ebb2e30f9b8da5b9a
Carlos Marques <carlos.mattos@fundingcircle.com>
2022-03-17 15:53:44 -0300

    Adds default logger option, adds report_interval_seconds key.

--- web/src/lib/default-config.js
+++ web/src/lib/default-config.js
@@ -7,12 +7,14 @@ import fs from 'fs'
 import logger from './logger'
 
 const defaultConfig = {
+  logger,
   log: (message, level = 'debug') => { logger.log({ level, message }) },
   prefix: '[judoscale] ',
   now: null,
   api_base_url: process.env.JUDOSCALE_URL,
   dyno: process.env.DYNO,
-  version: JSON.parse(fs.readFileSync('../../../package.json')).version
+  version: JSON.parse(fs.readFileSync('../../../package.json')).version,
+  report_interval_seconds: 10
 }
 
 export default defaultConfig
