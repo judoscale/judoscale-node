be97a37b44ce7b0a06bfdcb4ffc8e3f544cfcf96
Carlos Marques <carlos@mattosmarques.com>
2022-03-21 20:34:00 -0300

    Adds Handle Heroku router format test

--- web/src/test/request-metrics.test.js
+++ web/src/test/request-metrics.test.js
@@ -4,5 +4,12 @@ import RequestMetrics from '../lib/request-metrics'
 
 describe('RequestMetrics', () => {
   describe('queueTimeFromHeaders', () => {
+    test('Handle Heroku router format', () => {
+      const now = new Date('2012-12-12T12:12:12.012Z')
+      const requestStart = new Date(now.getTime() - 100).getTime().toString()
+      const headers = { 'x-request-start': requestStart }
+
+      expect(RequestMetrics.queueTimeFromHeaders(headers, now)).toBe(100)
+    })
   })
 })
