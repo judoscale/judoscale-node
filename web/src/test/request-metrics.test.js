cc558e2726f1414660786bbc3e3283478cb508ca
Carlos Marques <carlos@mattosmarques.com>
2022-03-21 20:34:52 -0300

    Adds Handle negative queue time test

--- web/src/test/request-metrics.test.js
+++ web/src/test/request-metrics.test.js
@@ -21,5 +21,13 @@ describe('RequestMetrics', () => {
 
       expect(RequestMetrics.queueTimeFromHeaders(headers, now)).toBe(100)
     })
+
+    test('Handle negative queue time', () => {
+      const now = new Date('2012-12-12T12:12:12.012Z')
+      const requestStart = new Date(now.getTime() + 100).getTime().toString()
+      const headers = { 'x-request-start': requestStart }
+
+      expect(RequestMetrics.queueTimeFromHeaders(headers, now)).toBe(0)
+    })
   })
 })
