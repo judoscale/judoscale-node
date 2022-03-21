6a3c265a7018a424fded9f75be8cc5a0e354362f
Carlos Marques <carlos@mattosmarques.com>
2022-03-21 20:28:09 -0300

    Refactors metric.js test

--- web/src/test/metric.test.js
+++ web/src/test/metric.test.js
@@ -2,13 +2,10 @@
 
 import Metric from '../lib/metric'
 
-test('Metric class', () => {
-  const identifier = 'some-identifier'
-  const time = new Date()
-  const value = '1234'
-  const metric = new Metric(identifier, time, value)
+const identifier = 'some-identifier'
+const time = new Date()
+const value = '1234'
+const metric = new Metric(identifier, time, value)
 
-  expect(metric.identifier).toBe(identifier)
-  expect(metric.time).toEqual(new Date(time.toUTCString()))
-  expect(metric.value).toBe(1234)
+describe('Metric', () => {
 })
