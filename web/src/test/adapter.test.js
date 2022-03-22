974a78d3da2751b544df21faf2440b714d3ff7b8
Carlos Marques <carlos@mattosmarques.com>
2022-03-22 00:41:03 -0300

    adapter.test.js

---
+++ web/src/test/adapter.test.js
@@ -0,0 +1,41 @@
+/* global test, expect, describe */
+
+import Adapter from '../lib/adapter'
+import defaultConfig from '../lib/default-config'
+
+const collectors = [{ a: 'collector' }]
+const adapter = new Adapter(collectors)
+
+describe('constructor', () => {
+  test('collectors property', () => {
+    expect(adapter.collectors).toEqual(collectors)
+  })
+
+  test('identifier property', () => {
+    expect(adapter.identifier).toEqual('judoscale-node')
+  })
+
+  test('adapter_version property', () => {
+    expect(adapter.adapter_version).toEqual(defaultConfig.version)
+  })
+
+  test('language_version property', () => {
+    expect(adapter.language_version).toEqual(process.version)
+  })
+})
+
+describe('asJson', () => {
+  const asJson = adapter.asJson()
+
+  test(`${adapter.identifier} as main key`, () => {
+    expect(asJson).toHaveProperty(adapter.identifier)
+  })
+
+  test('adapter_version with property value', () => {
+    expect(asJson).toHaveProperty(`${adapter.identifier}.adapter_version`, adapter.adapter_version)
+  })
+
+  test('language_version with property value', () => {
+    expect(asJson).toHaveProperty(`${adapter.identifier}.language_version`, adapter.language_version)
+  })
+})
