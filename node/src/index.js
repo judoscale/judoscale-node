0856eb5201ef880772502b278c4f19918bf6f19e
Carlos Marques <carlos@mattosmarques.com>
2022-03-29 16:55:13 -0300

    Export Modules

---
+++ node/src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview export all modules
+ * @author Carlos Marques
+ */
+
+import Api from './lib/api'
+import defaultConfig from './lib/default-config'
+import logger from './lib/logger'
+import Metric from './lib/Metric'
+import MetricStore from './lib/metrics-store'
+import Report from './lib/report'
+import Reporter from './lib/reporter'
+
+export {
+  Api
+  defaultConfig,
+  logger,
+  Metric,
+  MetricStore,
+  Report,
+  Reporter
+}
