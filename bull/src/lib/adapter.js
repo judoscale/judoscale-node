8f6b6ee0acbcdde2b2873968d79f6e5bf5104036
Carlos Marques <carlos@mattosmarques.com>
2022-03-31 16:17:35 -0300

    Bull adapter “adapter”

---
+++ bull/src/lib/adapter.js
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview Adapter metadata class
+ * @author Carlos Marques
+ */
+
+import { defaultConfig } from 'judoscale-node'
+
+class Adapter {
+  constructor(collectors) {
+    this.collectors = collectors
+    this.identifier = 'judoscale-bull'
+    this.adapter_version = defaultConfig.version
+    this.language_version = process.version
+  }
+
+  asJson() {
+    const data = {}
+    data[this.identifier] = {
+      adapter_version: this.adapter_version,
+      language_version: this.language_version
+    }
+
+    return data
+  }
+}
+
+export default Adapter
