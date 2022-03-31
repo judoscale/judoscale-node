80983af90f654fe9c69cc153f1f34b74c8dde007
Carlos Marques <carlos@mattosmarques.com>
2022-03-31 16:19:11 -0300

    Module added to adapter to pull Bull’s queues names from Redis

---
+++ bull/src/lib/queues.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Grab Bull's queues names from Redis
+ * @author Carlos Marques
+ */
+
+import redisClient from './lib/redis-client'
+
+export default (async () => {
+  const queueNameRegExp = /(.*):(.*):id/
+  const keys = await redisClient.keys('*:*:id')
+
+  return keys.map((key) => {
+    const match = queueNameRegExp.exec(key)
+
+    if (match) {
+      return {
+        prefix: match[1],
+        name: match[2]
+      }
+    }
+
+    return null
+  })
+})()
