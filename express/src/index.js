b35d6f3a233cc0f14709bdae0f141aa3f9d0c948
Carlos Marques <carlos@mattosmarques.com>
2022-04-01 15:54:17 -0300

    Proper imports.

--- express/src/index.js
+++ express/src/index.js
@@ -1,23 +1,14 @@
-import defaultConfig from 'judoscale-node-core'
-import MetricStore from 'judoscale-node-core'
-import Reporter from 'judoscale-node-core'
+import { defaultConfig } from 'judoscale-node-core'
+import { MetricsStore } from 'judoscale-node-core'
+import { Reporter } from 'judoscale-node-core'
 import requestMetrics from './lib/request-metrics'
 import WebMetricsCollector from './lib/web-metrics-collector'
 import Adapter from './lib/adapter'
-/**
- * Construct a middleware function for logging request queue time in whole milliseconds.
- * Log output looks like: [judoscale] queued=123
- *
- * @param {object} config - optional configuration
- *    config.log {function} - this function will be called with the queue time message
- *    config.now {Date} - represents the current time, used for testing
- *    config.prefix {string} - prefix log output (default "[judoscale] ")
- * @returns {function} - HTTP middleware
- */
+
 export default (config) => {
   const finalConfig = { ...defaultConfig, ...config }
 
-  const store = new MetricStore()
+  const store = new MetricsStore()
   const collectors = [new WebMetricsCollector(store)]
   const reporter = new Reporter()
 
