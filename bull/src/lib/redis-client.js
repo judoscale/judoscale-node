acb5fe4f02c80eeb7589a59a8db8def5a129da51
Adam McCrea <adam@adamlogic.com>
2022-07-15 16:07:32 -0400

    Fix logger

--- bull/src/lib/redis-client.js
+++ bull/src/lib/redis-client.js
@@ -4,7 +4,7 @@ import config from './config'
 export default (async () => {
   const client = createClient({ url: config.redisUrl })
 
-  client.on('error', (err) => config.log('Redis Client Error', err))
+  client.on('error', (err) => config.logger.error('Redis Client Error', err))
 
   await client.connect()
 
