44e041ad58fa8022223b4c1e6a6de99eaa1a0eac
Adam McCrea <adam@adamlogic.com>
2022-07-18 11:44:59 -0400

    core 1.0.4

--- node/src/lib/default-config.js
+++ node/src/lib/default-config.js
@@ -3,7 +3,7 @@ import getLogger from './logger'
 const defaultLogLevel = process.env.JUDOSCALE_LOG_LEVEL || 'info'
 
 export default {
-  version: '1.0.3',
+  version: '1.0.4',
   api_base_url: process.env.JUDOSCALE_URL,
   log_level: defaultLogLevel,
   dyno: process.env.DYNO,
