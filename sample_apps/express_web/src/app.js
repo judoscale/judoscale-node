5db52da670a372a901a4d46146be1e2a8fd944d0
Adam McCrea <adam@adamlogic.com>
2024-05-17 09:04:05 -0400

    Add worker to process jobs

--- sample_apps/express_web/src/app.js
+++ sample_apps/express_web/src/app.js
@@ -7,61 +7,67 @@ const app = express()
 const port = process.env.PORT || 5000
 const redisConfig = { connection: { host: '127.0.0.1', port: 6379 } }
 const redis = new Redis(redisConfig.connection)
+const judoscaleConfig = {
+  api_base_url: process.env.JUDOSCALE_URL || 'https://judoscale-node-sample.requestcatcher.com',
+}
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.use(
-  judoscale.default({
-    api_base_url: process.env.JUDOSCALE_URL || 'https://judoscale-node-sample.requestcatcher.com',
-  })
-)
-
+app.use(judoscale.default(judoscaleConfig))
 app.use(express.json()) // parse JSON bodies
 app.use(express.urlencoded({ extended: true })) // parse HTML forms
 
-app.get('/', async (req, res) => {
+app.get('/', handleIndex)
+app.post('/enqueue-jobs', handleEnqueueJobs)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+async function handleIndex(req, res) {
   const keys = await redis.keys('bull:*:id')
   const queueNames = keys.map((key) => key.split(':')[1])
 
   const queueStats = await Promise.all(
     queueNames.map(async (name) => {
       const queue = new Queue(name, redisConfig)
-      const queueEvents = new QueueEvents(name, redisConfig)
-      const jobCounts = await queue.getJobCounts()
-      // const latency = await queueEvents.getLatency()
-      const latency = 0
+      const jobCounts = await queue.getJobCounts('waiting', 'active')
+
+      // // Fetch the oldest job in the waiting state to calculate queue latency
+      // const oldestJobs = await queue.getJobs(['waiting'], 0, 0, true)
+      // // BUG: timestamp is when the job was created, so this breaks for delayed and retried jobs
+      // const latency = oldestJobs.length ? (Number(new Date()) - Number(new Date(oldestJobs[0].timestamp))) / 1000 : 0
+
       return {
         name,
-        jobs: jobCounts.waiting + jobCounts.active + jobCounts.delayed + jobCounts.completed + jobCounts.failed,
-        latency,
+        enqueuedJobs: jobCounts.waiting,
+        activeJobs: jobCounts.active,
       }
     })
   )
 
   res.render('index', { queues: queueStats })
-})
+}
 
-app.post('/enqueue-jobs', async (req, res) => {
+async function handleEnqueueJobs(req, res) {
   const queueName = req.body.queue || 'default'
-  const numJobs = parseInt(req.body.jobs, 10) || 1
-
+  const numJobs = Number(req.body.jobs) || 10
+  const jobData = { duration: Number(req.body.duration) || 10 }
   const queue = new Queue(queueName, redisConfig)
 
   try {
     const jobPromises = []
     for (let i = 0; i < numJobs; i++) {
-      jobPromises.push(queue.add('testJob', {}, { jobId: `testJob-${i + 1}` }))
+      jobPromises.push(queue.add('testJob', jobData))
     }
+
     await Promise.all(jobPromises)
+
     console.log(`${numJobs} jobs added to ${queueName} queue`)
     res.redirect('/')
   } catch (error) {
     console.error('Error enqueuing jobs:', error)
     res.status(500).send('Failed to enqueue jobs')
   }
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+}
---
+++ sample_apps/express_web/src/worker.js
@@ -0,0 +1,33 @@
+import { Worker } from 'bullmq'
+
+const redisOpts = { url: process.env.REDIS_URL || 'redis://127.0.0.1:6379' }
+const queueNames = ['default', 'urgent']
+
+const workers = queueNames.map((queueName) => {
+  console.log(`Starting worker for ${queueName} queue`)
+  return new Worker(queueName, handlerForQueue(queueName), { connection: redisOpts })
+})
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'))
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'))
+
+function handlerForQueue(queueName) {
+  return async (job) => {
+    console.log(`Processing job ${job.id} from ${queueName} queue for ${job.data.duration} seconds`)
+    await sleep(job.data.duration * 1000)
+    console.log(`Completed job ${job.id} from ${queueName} queue`)
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function gracefulShutdown(signal) {
+  console.log(`Received ${signal}, shutting down workers...`)
+  for (const worker of workers) {
+    await worker.close()
+  }
+
+  process.exit(0)
+}
