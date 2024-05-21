aef71af6a6c5d508ae59801774eba1791f361d54
Adam McCrea <adam@adamlogic.com>
2024-05-21 10:26:40 -0400

    Wire up reporting for BullMQ queue depth and queue busy metrics
    This required making the `collect` function async, along with the calling methods. We kept the `Reporter#start` method sync (not async) by resolving the `report` promise manually.

--- bullmq/src/bull-mq-metrics-collector.js
+++ bullmq/src/bull-mq-metrics-collector.js
@@ -12,29 +12,27 @@ class BullMQMetricsCollector extends WorkerMetricsCollector {
     this.queueNames = new Set()
   }
 
-  collect() {
-    // if (this.queueNames.size == 0) await this.fetchQueueNames()
+  async collect() {
+    if (this.queueNames.size == 0) await this.fetchQueueNames()
 
-    // let metrics = []
+    let metrics = []
 
-    // for (const queueName of queueNames) {
-    //   const queue = new Queue(queueName, this.redis.config)
-    //   const jobCounts = await queue.getJobCounts('waiting', 'active')
+    for (const queueName of this.queueNames) {
+      const queue = new Queue(queueName, this.redis.config)
+      const jobCounts = await queue.getJobCounts('waiting', 'active')
 
-    //   metrics.push(new Metric('qd', new Date(), jobCounts.waiting, queueName))
-    //   metrics.push(new Metric('busy', new Date(), jobCounts.active, queueName))
-    // }
+      metrics.push(new Metric('qd', new Date(), jobCounts.waiting, queueName))
+      metrics.push(new Metric('busy', new Date(), jobCounts.active, queueName))
+    }
 
-    // return metrics
-    console.log('returning no metrics for now')
-    return []
+    return metrics
   }
 
   async fetchQueueNames() {
     const redisKeys = []
     let cursor = '0'
     do {
-      const reply = await redis.scan(cursor, 'MATCH', 'bull:*:id')
+      const reply = await this.redis.scan(cursor, 'MATCH', 'bull:*:id')
       cursor = reply[0]
       redisKeys.push(...reply[1])
     } while (cursor !== '0')
--- node-core/src/lib/reporter.js
+++ node-core/src/lib/reporter.js
@@ -27,9 +27,9 @@ class Reporter {
 
       forever((next) => {
         setTimeout(() => {
-          this.report(adapter, config, collectors)
-
-          next()
+          this.report(adapter, config, collectors).then(() => {
+            next()
+          })
         }, config.report_interval_seconds * 1000)
       })
     }
@@ -43,8 +43,8 @@ class Reporter {
     return this.started
   }
 
-  report(adapter, config, collectors) {
-    const metrics = collectors.map((collector) => collector.collect()).flat()
+  async report(adapter, config, collectors) {
+    const metrics = (await Promise.all(collectors.map((collector) => collector.collect()))).flat()
     const report = new Report(adapter, config, metrics)
     config.logger.info(`[Judoscale] Reporting ${report.metrics.length} metrics`)
 
