cb4043655b3b6ae15fddd9c3e340fe042f1edb0d
Adam McCrea <adam@adamlogic.com>
2024-05-28 15:54:45 -0400

    fix: Gracefully handle missing queue time for fastify

--- fastify/test/plugin.test.js
+++ fastify/test/plugin.test.js
@@ -1,6 +1,5 @@
 const fastify = require('fastify')
 const { Judoscale, plugin } = require('../src/plugin')
-const { MetricsStore } = require('judoscale-node-core')
 
 describe('Judoscale Fastify Plugin', () => {
   let app
@@ -33,5 +32,17 @@ describe('Judoscale Fastify Plugin', () => {
     // Queue time should be 100-200ms depending how long the test takes to run
     expect(metrics[0].value).toBeGreaterThanOrEqual(100)
     expect(metrics[0].value).toBeLessThan(200)
+  })
+
+  test('gracefully handles missing queue time', async () => {
+    await app.inject({
+      method: 'GET',
+      url: '/',
+      headers: {},
+    })
+
+    const metrics = Judoscale.adapters[0].collector.collect()
+
+    expect(metrics).toEqual([])
   })
 })
