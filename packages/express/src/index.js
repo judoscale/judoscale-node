419999ad69216d180624d796e610d4d95d95fa48
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-09-02 11:52:29 -0300

    Track app time with Express

--- packages/express/src/index.js
+++ packages/express/src/index.js
@@ -4,16 +4,26 @@ const packageInfo = require('../package.json')
 const store = new MetricsStore()
 
 function middleware(judoscale) {
-  return ({ headers }, _res, next) => {
+  return (req, res, next) => {
     const now = judoscale.config.now || new Date()
-    const queueTime = requestMetrics.queueTimeFromHeaders(headers, now)
+    const queueTime = requestMetrics.queueTimeFromHeaders(req.headers, now)
+    const requestId = requestMetrics.requestId(req.headers)
 
     if (queueTime !== null) {
-      judoscale.config.logger.debug(
-        `[Judoscale] queue_time=${queueTime}ms request_id=${requestMetrics.requestId(headers)}`
-      )
+      judoscale.config.logger.debug(`[Judoscale] queue_time=${queueTime}ms request_id=${requestId}`)
       store.push('qt', queueTime)
     }
+
+    const startTime = process.hrtime.bigint();
+
+    res.on('finish', () => {
+      const endTime = process.hrtime.bigint();
+      const appTimeNs = endTime - startTime
+      const appTimeMs = Math.floor(Number(appTimeNs) / 1_000_000)
+
+      store.push('at', appTimeMs)
+      judoscale.config.logger.debug(`[Judoscale] app_time=${appTimeMs}ms request_id=${requestId}`)
+    })
 
     next()
   }
--- packages/express/src/test/index.test.js
+++ packages/express/src/test/index.test.js
@@ -25,10 +25,13 @@ describe('middleware', () => {
     expect(response.text).toBe('Middleware test')
 
     const metrics = Judoscale.adapters[0].collector.collect()
-    expect(metrics.length).toEqual(1)
+    expect(metrics.length).toEqual(2)
     // Queue time should be 100-200ms depending how long the test takes to run
+    expect(metrics[0].identifier).toEqual('qt')
     expect(metrics[0].value).toBeGreaterThanOrEqual(100)
     expect(metrics[0].value).toBeLessThan(200)
+    expect(metrics[1].identifier).toEqual('at')
+    expect(metrics[1].value).toBeGreaterThanOrEqual(0)
   })
 
   test('gracefully handles missing queue time', async () => {
@@ -36,6 +39,8 @@ describe('middleware', () => {
     expect(response.statusCode).toBe(200)
 
     const metrics = Judoscale.adapters[0].collector.collect()
-    expect(metrics).toEqual([])
+    // Only app time is tracked, queue time isn't.
+    expect(metrics.length).toEqual(1)
+    expect(metrics[0].identifier).toEqual('at')
   })
 })
